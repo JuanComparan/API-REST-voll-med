e35ba15df9661500de402eb519d4691cc3e34daf
JuanComparan <valeriocomparanj@gmail.com>
2024-07-05 08:17:47 -0600

    commit from intellij

--- src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
+++ src/main/java/med/voll/api/domain/consulta/ConsultaRepository.java
@@ -3,8 +3,6 @@ package med.voll.api.domain.consulta;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
-//primer commit en intellij
-
 @Repository
 public interface ConsultaRepository extends JpaRepository<Consulta, Long> {
 }
